aa0cf5f495b9946738cd7f01feb5b7adf17cec3a
Callum Gray <callumgray.cg@gmail.com>
2022-02-19 13:56:55 +0000

    Added server and client

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+const express = require('express');
 
+const app = express();
+app.use(express.static('client'));
+
+// app.get('/', (req, res) => {
+//     res.end("hello world");
+// });
+
+app.listen(3000);
