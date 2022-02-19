44c476c7810b5f35587ed97762a9c2129060dc1e
CharlieCarpenter13 <92162437+CharlieCarpenter13@users.noreply.github.com>
2022-02-19 18:10:34 +0000

    adding bar

--- distances.js
+++ distances.js
@@ -1,33 +1,38 @@
 // h
 const axios = require('axios');
+const sqlite3 = require('sqlite3').verbose();
 
-function distances(place1, place2){
-    let url = 'https://maps.googleapis.com/maps/api/distancematrix/json?destinations=' + place1 + '&origins=' + place2 + '&units=imperial&key=AIzaSyAaKfCdw4jDuY1rZcH_hMW3nCwKfM8uWLI'
-    axios.get(url)
-    .then(res => {
-
-        const dist = res.data;
-        console.log(dist)
-        return dist.rows[0].elements;
-    })
-    .catch(err => {
-        console.log('Error: ', err.message);
-    });
-}
+let db = new sqlite3.Database('./data.sql');
 
 function work_out_all(new_bar, database_bar){
-    console.log("sdf")
     for (let i = 0; i < database_bar.length; i++) {
         let bar = database_bar[i];
-        console.log(bar.coords)
-        let dist = distances(new_bar.coords, bar.coords);
-        console.log(dist);
+
+        let url = 'https://maps.googleapis.com/maps/api/distancematrix/json?destinations=' + new_bar.coords + '&origins=' + bar.coords + '&units=imperial&key=AIzaSyAaKfCdw4jDuY1rZcH_hMW3nCwKfM8uWLI'
+        axios.get(url)
+        .then(res => {
+    
+            const dist = res.data.rows[0].elements;
+            console.log(dist);
+
+            let data_for_db = [new_bar.name, bar.name, dist[0].distance, dist[0].duration]
+
+            // adding it to the database
+            db.run(`INSERT INTO distances(bar1, bar2, distance, duration) VALUES(?,?,?,?)`, data_for_db, function(err) {
+                if (err) {
+                  return console.log(err.message);
+                }
+                // get the last insert id
+                console.log(`A row has been inserted with rowid ${this.lastID}`);
+              });
+
+        })
+        .catch(err => {
+            console.log('Error: ', err.message);
+        });
+
 
     }
-
-    // loop through other bars are work out how far.
-    // loop
-    // store
 }
 
 module.exports = {
