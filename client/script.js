6188f77cc97fca66ed3db56eb16e4a11718bc7ad
callum-gg <81529257+callum-gg@users.noreply.github.com>
2022-04-19 13:49:29 +0100

    Ensure >1 location is selected

--- client/script.js
+++ client/script.js
@@ -18,30 +18,6 @@ function GetBarData(bar) {
     // })
 }
 
-function GetBarOrder(bars, start, end) {
-    fetch('/bar/order', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({bars, start, end})
-    }).then(resp => {
-        //response = ordered array of college bars {name, time to next, coords}
-        let url = `https://www.google.com/maps/embed/v1/directions?
-key=AIzaSyDvBY_k1J6aWeB71ReE8EuC08jrf-dJYJ0
-&mode=walking
-&origin=${resp[0].coords}
-&destination=${resp[resp.length-1].coords}
-&waypoints=`;
-        for (var i=1; i<resp.length-1; i++) {
-            url = `${url}${resp[i].coords}|`
-        }
-        document.querySelector('#map').setAttribute("src", url)
-    })//.catch(err => {
-
-    //})
-}
-
 function add_new_bar(new_bar) {
     fetch('/bar/new', {
         method: 'POST',
@@ -107,39 +83,42 @@ function update_bar_crawl(){
         }
     };
 
-    // workking out the persone tisr fdjgafmdf`
-    let percent = (bars_check.length / number_bars)*100;
-    document.getElementById("bar_percentage").innerText = percent + "%";
+    if (bars_check.length > 1) {
 
-    if (percent == 100){
-        alert("They are here if you need help here is a number too call:  0344 209 0754")
-    }
+        // workking out the persone tisr fdjgafmdf`
+        let percent = (bars_check.length / number_bars)*100;
+        document.getElementById("bar_percentage").innerText = percent + "%";
 
-    fetch('/bar/order', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({bars: bars_check})
-    }).then(resp => resp.json()).then(resp => {
-        //response = ordered array of college bars
-        let url = `https://www.google.com/maps/embed/v1/directions?
+        if (percent == 100){
+            alert("They are here if you need help here is a number too call:  0344 209 0754")
+        }
+
+        fetch('/bar/order', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({bars: bars_check})
+        }).then(resp => resp.json()).then(resp => {
+            //response = ordered array of college bars
+            let url = `https://www.google.com/maps/embed/v1/directions?
 key=AIzaSyDvBY_k1J6aWeB71ReE8EuC08jrf-dJYJ0
 &mode=walking
 &origin=${resp[0].coords}
-&destination=${resp[resp.length-1].coords}`
-//&waypoints=`;
-        if (resp.length > 2) {
-            url = `${url}&waypoints=`
-            for (var i=1; i<resp.length-1; i++) {
-                url = `${url}${resp[i].coords}|`
+&destination=${resp[resp.length-1].coords}`;
+            if (resp.length > 2) {
+                url = `${url}&waypoints=`
+                for (var i=1; i<resp.length-1; i++) {
+                    url = `${url}${resp[i].coords}|`
+                }
             }
-        }
+            document.querySelector('#map').setAttribute("src", url.slice(0, -1))
+        })//.catch(err => {
 
-        document.querySelector('#map').setAttribute("src", url.slice(0, -1))
-    })//.catch(err => {
-
-    //})
+        //})
+    } else {
+        alert('You need to have more than 1 bar. It\'s a bar crawl.');
+    }
 }
 
 function add_bar(){
