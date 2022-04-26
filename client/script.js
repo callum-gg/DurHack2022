36f165612f0f054bdb8d096a50666b4195ae766a
callum-gg <81529257+callum-gg@users.noreply.github.com>
2022-04-26 11:36:36 +0100

    Added share functions

--- client/script.js
+++ client/script.js
@@ -1,13 +1,6 @@
-// function post() {
-//     fetch('/', {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({data: 'omg this is cool'})
-//     })
-// }
 let number_bars = 0;
+let current_route, shareid
+
 function GetBarData(bar) {
     fetch(`/bar/get?bar=${bar}`, {
         method: 'GET',
@@ -42,10 +35,11 @@ function show_bar_choice(){
 
 window.addEventListener('load', function () {
     // This loads the bars to begin
-    this.fetch(`/user/get`, {
+    fetch(`/user/get`, {
         method: 'GET'
     }).then(resp => resp.json()).then(resp => {
         if (resp.loggedIn) {
+            shareid = resp.shareid;
             document.getElementById('main-button').innerText = "College Bars";
             document.getElementById('main-button').setAttribute("data-bs-target", "#barsModal");
 
@@ -55,6 +49,11 @@ window.addEventListener('load', function () {
                 create_bar_selector(resp)
         
             });
+
+            let urlParams = new URLSearchParams(window.location.search);
+            if (urlParams.get('share')) {
+                checkShared(urlParams.get('share'));
+            }
         }
     })
 });
@@ -68,13 +67,17 @@ function create_bar_selector(bars){
         number_bars += 1;
 
         document.getElementById("choisingbars").innerHTML += `
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="`+bar+`" id="bar_`+i+`" checked>
-          <label class="form-check-label" for="flexCheckChecked">
-            `+bar+`
-          </label>
-        </div>
-        `
+        <div class="bars-form">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="`+bar+`" id="bar_${i}" checked>
+                <label class="form-check-label" for="bar+${i}">${bar}</label>
+            </div>
+            <div class="form-check-actions">
+                ${""/*<button class="form-button" onclick="editLocation('${bar}');"><i class="fa-solid fa-pencil"></i></button>*/}
+                <button class="form-button" onclick="deleteLocation('${bar}')"><i class="fa-solid fa-trash"></i></button>
+                <button class="form-button" onclick='shareLocation(\`${JSON.stringify(bars[i])}\`)'><i class="fa-solid fa-share"></i></button>
+            </div>
+        </div>`
     }
 
     
@@ -120,8 +123,10 @@ key=AIzaSyDvBY_k1J6aWeB71ReE8EuC08jrf-dJYJ0
                 for (var i=1; i<resp.length-1; i++) {
                     url = `${url}${resp[i].coords}|`
                 }
+                url = url.slice(0, -1);
             }
-            document.querySelector('#map').setAttribute("src", url.slice(0, -1))
+            document.querySelector('#map').setAttribute("src", url)
+            current_route = resp;
         })//.catch(err => {
 
         //})
@@ -144,4 +149,85 @@ function add_bar(){
 
     location.reload();
    
+}
+
+function deleteLocation(bar) {
+    fetch('/bar/delete', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({bar})
+    }).then(() => {
+        location.reload();
+    })
+}
+
+function shareLocation(bar) {
+    bar = JSON.parse(bar);
+    $('#barsModal').modal("toggle");
+    fetch('/share/location', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({location: bar})
+    }).then(resp => resp.json()).then(resp => {
+        document.querySelector('#shareModalLabel').innerText = "Location Link:";
+        $('#shareModal').modal("toggle");
+        document.getElementById("shareLink").value = `${window.location.hostname}/?share=${resp.shareid}`;
+    })
+}
+
+function shareRoute() {
+    if (current_route) {
+        fetch('/share/route', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({route: current_route})
+        }).then(resp => resp.json()).then(resp => {
+            document.querySelector('#shareModalLabel').innerText = "Location Link:";
+            $('#shareModal').modal("toggle");
+            document.getElementById("shareLink").value = `${window.location.hostname}/?share=${resp.shareid}`;
+        })
+    } else {
+        alert('You need to create a route first!');
+    }
+}
+
+function checkShared(shareid) {
+    fetch(`/share/get?shareid=${shareid}`, {
+        method: 'GET'
+    }).then(resp => resp.json()).then(resp => {
+        if (resp.type === "location") {
+            document.querySelector('#bar_name').value = resp.location.name;
+            document.querySelector('#longlat').value = resp.location.coords;
+            $('#barsModal').modal('show');
+        } else if (resp.type === "route") {
+            let url = `https://www.google.com/maps/embed/v1/directions?
+key=AIzaSyDvBY_k1J6aWeB71ReE8EuC08jrf-dJYJ0
+&mode=walking
+&origin=${resp.route[0].coords}
+&destination=${resp.route[resp.route.length-1].coords}`;
+            if (resp.route.length > 2) {
+                url = `${url}&waypoints=`
+                for (var i=1; i<resp.route.length-1; i++) {
+                    url = `${url}${resp.route[i].coords}|`
+                }
+                url = url.slice(0, -1);
+            }
+            document.querySelector('#map').setAttribute("src", url)
+            current_route = resp.route;
+        }
+    })
+}
+
+function copyShareLink() {
+  var copyText = document.getElementById("shareLink");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
+  document.querySelector('#linkAlert').style.opacity = 1;
 }
